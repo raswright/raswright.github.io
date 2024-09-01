ac7f304ab9a8a0735f3b32dd6219fac2b1d9f392
raswright <rasean0310@gmail.com>
2024-08-31 21:03:24 -0400

    added logo

--- projects/part1/script.js
+++ projects/part1/script.js
@@ -24,18 +24,3 @@ function validateForm() {
 document.getElementById("contactForm").onsubmit = function() {
     return validateForm();
 };
-
-// Slideshow script
-let slideIndex = 0;
-showSlides();
-
-function showSlides() {
-    let slides = document.getElementsByClassName("mySlides");
-    for (let i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none"; // Hide  slides
-    }
-    slideIndex++;
-    if (slideIndex > slides.length) {slideIndex = 1} // Reset to first slide if at the end
-    slides[slideIndex - 1].style.display = "block"; // Display the current slide
-    setTimeout(showSlides, 3000); // Change slide every 3 seconds
-}
