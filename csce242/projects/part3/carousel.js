b64a3dcead62f74c77a54d1c1c10660f14c7c891
raswright <rasean0310@gmail.com>
2024-09-15 01:04:24 -0400

    updated project part 3

--- csce242/projects/part3/carousel.js
+++ csce242/projects/part3/carousel.js
@@ -2,7 +2,7 @@ let currentItem = 0;
 const items = document.querySelectorAll('.carousel-item');
 
 setInterval(() => {
-    items[currentItem].style.display = 'none'; // Hide current item
-    currentItem = (currentItem + 1) % items.length; // Move to the next
-    items[currentItem].style.display = 'block'; // Show next item
-}, 3000); // Switch every 3 seconds
+    items[currentItem].style.display = 'none'; // hides the current item
+    currentItem = (currentItem + 1) % items.length; // moves to the next
+    items[currentItem].style.display = 'block'; // shows next item
+}, 3000); // switches every 3 seconds
